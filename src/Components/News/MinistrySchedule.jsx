178d10b9ea9ec4859467c1b7d54c5818b4248b2a
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-09-08 06:31:34 +0100

    schedules updated

--- src/Components/News/MinistrySchedule.jsx
+++ src/Components/News/MinistrySchedule.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Schedule from './Schedule';
+import schedule from '../../Assets/schedule.jpg'
 
 const MinistrySchedule = () => {
-    return(
-        <div>
-            Ministry Schedule
+    return (
+        <div className='aboutpg'>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Mukta&display=swap');
+            </style>
+
+            <div className='abouthead'>
+                <div className="aboutheadimg">
+                    <img src={schedule} alt="" />
+                </div>
+
+            </div>
+
+            <div style={{
+                width: '100vw',
+                height: 'auto'
+            }}>
+                <Schedule/>
+            </div>
+        
+
+
+
         </div>
     )
 }
--- src/Components/News/Schedule.jsx
+++ src/Components/News/Schedule.jsx
@@ -3,16 +3,24 @@ import React from 'react'
 const Schedule = () => {
   return (
     <div>
-        <h2>Schedule</h2>
-        <div className="google-calendar-embed">
+        <h2>Check calendar for Events</h2>
+
+        <div 
+        style={{
+            width: '100%',
+            height: '50vh'
+        }}
+        >
             
             <iframe
-            src="https://calendar.google.com/calendar/embed?src=ec38d9607a869f008514b4fc083dcaffc0cbb6514b9ae064a8e744906b35d3e2%40group.calendar.google.com&ctz=Africa%2FLagos"
-            style={{ border: '0' }}
-            width="800"
-            height="600"
+            src="https://calendar.google.com/calendar/embed?src=ec38d9607a869f008514b4fc083dcaffc0cbb6514b9ae064a8e744906b35d3e2%40group.calendar.google.com&ctz=Europe%2FAmsterdam"
+            style={{ border: '0', display: 'block', margin: "auto" }}
+            width="100%"
+            height="100%"
             frameborder="0"
             scrolling ="no"
+            title='calendar'
+            className='calendar'
             >
 
             </iframe>
@@ -22,4 +30,4 @@ const Schedule = () => {
   )
 }
 
-export default Schedule
+export default Schedule;
