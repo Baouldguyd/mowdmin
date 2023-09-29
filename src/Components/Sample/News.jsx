d4a67e7930f9ba959c8fc9228ac29fdf599c93cf
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-09-29 16:21:15 +0100

    Update News.jsx

--- src/Components/Sample/News.jsx
+++ src/Components/Sample/News.jsx
@@ -17,7 +17,7 @@ const News = () => {
         <div className='newsButtonDiv'>
 
           <Link to='/newspage' >
-            <button className='pixels'><FaNewspaper/>  Announcements </button>
+            <button className='pixels'><FaNewspaper/>  AEvents </button>
             </Link>
             <Link to='/ministryschedule' >
             <button><FaChurch/> MINISTRY SCHEDULE</button>
