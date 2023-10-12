8d1009d205d770b41400996ab7464d6c093a0db0
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-10-12 16:42:36 +0100

    update gift page

--- src/Components/Pages/GiftPage.jsx
+++ src/Components/Pages/GiftPage.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import gift from '../../Assets/gift.png'
+import paypal from '../../Assets/paypal.png'
+import stripe from '../../Assets/stripe.png'
+
+
+const giftStyle = {
+  width: '40vw',
+  height: '4rem',
+  display: 'block',
+  margin : 'auto',
+  marginTop : '10px',
+  backgroundColor: 'white',
+  borderRadius : '10px'
+}
+
 
 const GiftPage = () => {
   return (
-    <div>GiftPage</div>
+    <div className='aboutpg'>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Mukta&display=swap');
+      </style>
+
+      <div className='abouthead'>
+          <div className="aboutheadimg">
+            <img src={gift} alt="" />
+          </div>
+       
+      </div>
+
+      <div>
+        <h2>Make a Gift</h2>
+        <p style={{ textAlign: 'center' }}>Use any of our payment methods below</p>
+      </div>
+
+      <div style={{
+        width: '100vw',
+        display: 'flex'
+        
+      }}>
+        <img style={giftStyle} src={stripe} alt="" />
+        <img style={giftStyle} src={paypal} alt="" />
+      </div>
+
+    </div>
   )
 }
 
