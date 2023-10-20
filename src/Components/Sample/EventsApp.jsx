e7365610dbdb1df0c238f6a0bbc9726aa907aae7
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-10-20 08:17:13 +0100

    update crusade

--- src/Components/Sample/EventsApp.jsx
+++ src/Components/Sample/EventsApp.jsx
@@ -96,7 +96,7 @@ const EventsApp = () => (
       </div>
     </Link>
 
-    <Link to="/events/crusade">
+    
       <div
         style={{
           display: "flex",
@@ -115,9 +115,43 @@ const EventsApp = () => (
             deliver His people. For it is written, "WHEN THE SON DELIVERS YOU,
             YOU WILL REALLY BE FREE." (JOHN 8vs36).
           </p>
+
+          <p>
+          We have received the commission: “Go into all the world and preach the
+          gospel to all creatures”. Mark 16, verse 15 “. This brought us
+          together to organize an open-air evangelization in the city of Hamm
+          (Westf) NRW for the first time in 2017. It is a moment that we meet to
+          praise God the Almighty. There is always inspired gospel music, by
+          different gospel singers who support the event.
+        </p>
+
+        <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          width: "75vw",
+          margin: "0 auto",
+        }}
+      >
+        <button>
+          <a href="https://www.mowdministries.org/galerie">
+            Past Open Air Evangelisation
+          </a>
+        </button>
+
+        <button>
+          <a href="https://www.mowdministries.org/events">
+            Coming Open Air Evangelisation
+          </a>
+        </button>
+      </div>
+
+
+          
         </div>
       </div>
-    </Link>
+    
 
     
       <div
