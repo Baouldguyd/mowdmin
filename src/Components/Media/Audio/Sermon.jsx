a03821015e475f2291f20f717f5ecf49923d2fca
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-11-13 18:17:33 +0100

    updated the sermon page

--- src/Components/Media/Audio/Sermon.jsx
+++ src/Components/Media/Audio/Sermon.jsx
@@ -1,50 +1,53 @@
-import React from 'react'
-import { FaPlay } from 'react-icons/fa';
+import React from "react";
+import { FaPlay } from "react-icons/fa";
+
+const sermonDetails = [
+  {
+    sermonTitle: "Dayspring from on high hath visited us: John 5v8.",
+    sermonLink: "https://www.facebook.com/maurille.osse",
+  },
+
+  {
+    sermonTitle: "My Rood shall blossom because I am a chosen one",
+    sermonLink: "https://www.instagram.com/mowdministries_e.v/",
+  },
+
+  {
+    sermonTitle: "Everyday people Everyday life.",
+    sermonLink: "https://twitter.com/Mowdministries",
+  },
+
+  {
+    sermonTitle: "Hour of the Truth.",
+    sermonLink: "https://t.me/mowdministriesev",
+  },
+];
 
 const Sermon = () => {
   return (
-    <div className='audioPage'>
-        <div className="audioList">
+    <div className="audioPage">
+      <div className="audioList">
+        {sermonDetails.map((item) => (
+          <div>
             <div className="audioFile">
-                <p>The Power in the blood of Jesus</p>
+              <p>{item.sermonTitle}</p>
               <div className="playDiv">
-              <button> <FaPlay className='playIcon'/>  </button>
+              
+                <button>
+                <a href={item.sermonLink}>
+                  <FaPlay className="playIcon" />{" "}
+                  </a>
+                </button>
+              
             </div>
-          </div>
-          
-        </div>
 
-        <div className="audioList">
-            <div className="audioFile">
-                <p>The Saving Grace of Jesus Christ</p>
-              <div className="playDiv">
-              <button> <FaPlay className='playIcon'/>  </button>
             </div>
+            
           </div>
-          
-        </div>
-
-        <div className="audioList">
-            <div className="audioFile">
-                <p>Unlocking Divine Virtues</p>
-              <div className="playDiv">
-              <button> <FaPlay className='playIcon'/>  </button>
-            </div>
-          </div>
-          
-        </div>
-
-        <div className="audioList">
-            <div className="audioFile">
-                <p>Unlocking Supernatural Gifts</p>
-              <div className="playDiv">
-              <button> <FaPlay className='playIcon' />  </button>
-            </div>
-          </div>
-          
-        </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sermon;
