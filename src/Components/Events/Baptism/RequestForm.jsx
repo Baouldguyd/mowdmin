0c7b12ec7dc4c8a63bceaad32f033fe5fb9322b9
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-11-17 12:24:56 +0100

    update prayer request

--- src/Components/Events/Baptism/RequestForm.jsx
+++ src/Components/Events/Baptism/RequestForm.jsx
@@ -1,78 +1,79 @@
-import React, { useState } from 'react';
-import {  useForm, ValidationError } from '@formspree/react';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import axios from "axios";
 
-import styles from './Baptism.module.css';
-
+import styles from "./Baptism.module.css";
 
 function RequestForm() {
- 
-
   const [prayer, setPrayer] = useState({
-    pRequest: ''
+    pRequest: "",
   });
 
- 
-
- 
   const handlePrayerRequest = (event) => {
     setPrayer({
-      pRequest: event.target.value
+      pRequest: event.target.value,
     });
   };
 
-  const handleSubmitRequest = async (event) => {   
-      
+  const handleSubmitRequest = async (event) => {
     event.preventDefault();
-  
+
     const requestData = {
       pRequest: event.target.prRequest.value,
+     
     };
-  
-    try {
-      const response = await axios.post(`https://prayer-request.onrender.com/api/v1/request/createRequest`
-      , requestData , {
-        
-      });
-      const apiResponse = response.data.responseMessage
-  
- 
-      if (response.data.responseCode === '00') {
-        console.log('Request created Succesfully');
-        console.log(response);
-        console.log(apiResponse);
-      
-        
-        alert(`Your Prayer Request has been created succesfully . We will pray for you.`)
-        
 
+    try {
+      // Send data to your first API (prRequest API)
+      const responseAPI = await axios.post(
+        "https://prayer-request.onrender.com/api/v1/request/createRequest",
+        requestData
+      );
+
+      if (responseAPI.data.responseCode === "00") {
+        console.log("Request created successfully");
+        console.log(responseAPI);
+        alert(
+          `Your Prayer Request has been created successfully. We will pray for you.`
+        );
       } else {
-        
-        console.log('Failed to send prayer request');
+        console.log("Failed to send prayer request to prRequest API");
       }
     } catch (error) {
-      console.error("An error occurred:", error);
+      console.error("An error occurred while sending data to prRequest API:", error);
     }
-   
-    
-    const form = document.getElementsByName('form')
-    form.style.display = 'none'
 
     try {
-      const submitInfo =  await axios.post('https://formspree.io/f/xnqkyaze')
-      
+      // Send data to Formspree
+      const responseFormspree = await axios.post("https://formspree.io/f/xnqkyaze", {
+        name: requestData.name,
+        country: requestData.country,
+        state: requestData.state,
+        address: requestData.address,
+        phone: requestData.phone,
+        email: requestData.email,
+        prRequest: requestData.pRequest,
+      });
 
+      if (responseFormspree.status === 200) {
+        console.log("Form data sent to Formspree successfully");
+        return <p>Thank you. We will join you in Prayers</p>
+      } else {
+        console.log("Failed to send form data to Formspree");
+      }
     } catch (error) {
-      
+      console.error("An error occurred while sending data to Formspree:", error);
     }
-  }
-  
-  
-  
-  return (
-    <form onSubmit={handleSubmitRequest} className={styles.form} >
 
-      <input
+    // Optionally hide the form after submission
+    const form = document.getElementsByName("form")[0];
+    form.style.display = "none";
+  };
+
+  return (
+    <form onSubmit={handleSubmitRequest} className={styles.form} name="form">
+      {/* ... (rest of your form inputs) */}
+       <input
         type="text"
         id="name"
         name="name"
@@ -81,7 +82,7 @@ function RequestForm() {
         required
       />
 
-<input
+      <input
         id="country"
         type="text"
         name="country"
@@ -89,7 +90,7 @@ function RequestForm() {
         required
       />
 
-<input
+      <input
         id="state"
         type="text"
         name="state"
@@ -97,7 +98,7 @@ function RequestForm() {
         required
       />
 
-<input
+      <input
         id="address"
         type="text"
         name="address"
@@ -105,13 +106,7 @@ function RequestForm() {
         required
       />
 
-<input
-        type="tel"
-        name="phone"
-        id="phone"
-        placeholder="Phone"
-        required
-      />
+      <input type="tel" name="phone" id="phone" placeholder="Phone" required />
 
       <input
         id="email"
@@ -121,16 +116,7 @@ function RequestForm() {
         required
       />
 
-      <ValidationError
-        prefix="Email"
-        field="email"
-        
-      />
-
-      
-
-
-
+      <ValidationError prefix="Email" field="email" />
 
       <input
         type="text"
@@ -142,13 +128,14 @@ function RequestForm() {
         value={prayer.pRequest}
         onChange={handlePrayerRequest}
       />
-
-      <button type="submit" className={styles.bn13} >
+      {/* ... (rest of your form inputs) */}
+      <button type="submit" className={styles.bn13}>
         SUBMIT
       </button>
     </form>
   );
-    
 }
 
 export default RequestForm;
+
+
