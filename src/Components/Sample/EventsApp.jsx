87f1b275877b1d3f08792f4e271aeee1bb13fa10
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-12-29 11:34:12 +0100

    update convention

--- src/Components/Sample/EventsApp.jsx
+++ src/Components/Sample/EventsApp.jsx
@@ -134,16 +134,19 @@ const EventsApp = () => (
       </div>
 
       <div>
-        <p
-        style={sympo}
-        >
-          We have received the commission: “Go into all the world and preach the gospel to all creatures”. Mark 16, verse 15 “. This brought us together to organize an open-air evangelization in the city of Hamm (Westf.) NRW for the first time in 2017. It is a moment that we meet to praise God the Almighty. There is always inspired gospel music, by different gospel singers who support the event.
+        <p style={sympo}>
+          We have received the commission: “Go into all the world and preach the
+          gospel to all creatures”. Mark 16, verse 15 “. This brought us
+          together to organize an open-air evangelization in the city of Hamm
+          (Westf.) NRW for the first time in 2017. It is a moment that we meet
+          to praise God the Almighty. There is always inspired gospel music, by
+          different gospel singers who support the event.
         </p>
 
-        <p
-        style={sympo}
-        >
-          Through the annual "open air evangelization", GOD is manifested to heal and deliver His people. For it is written, "WHEN THE SON DELIVERS YOU, YOU WILL REALLY BE FREE." (JOHN 8vs36).
+        <p style={sympo}>
+          Through the annual "open air evangelization", GOD is manifested to
+          heal and deliver His people. For it is written, "WHEN THE SON DELIVERS
+          YOU, YOU WILL REALLY BE FREE." (JOHN 8vs36).
         </p>
         <div
           style={{
@@ -161,18 +164,16 @@ const EventsApp = () => (
           </button>
 
           <p
-          style={{
-            textAlign: "center",
-            marginBottom: "1.3rem",
-            fontWeight: 'bolder'
-          }}
-        >
-          COMING OPEN AIR EVANGELISATION BEGINS IN{" "}
-        </p>
+            style={{
+              textAlign: "center",
+              marginBottom: "1.3rem",
+              fontWeight: "bolder",
+            }}
+          >
+            COMING OPEN AIR EVANGELISATION BEGINS IN{" "}
+          </p>
 
-        <CountdownTimer targetDate={targetDate} />
-
-          
+          <CountdownTimer targetDate={targetDate} />
 
           <button>
             <a href="https://www.mowdministries.org/events">
@@ -316,40 +317,28 @@ const EventsApp = () => (
           the blood of our Lord Jesus Christ for Sanctification and healing.
         </p>
         <p>
-        In the beginning of the new month and of course follow by the program, 
-"The voice of the presence of God ", on the first Sunday of the month we gather to break the bread and take the cup as our Lord Jesus Christ commanded us to do this in His remembrance in Luke 22vs 19.
-
+          In the beginning of the new month and of course follow by the program,
+          "The voice of the presence of God ", on the first Sunday of the month
+          we gather to break the bread and take the cup as our Lord Jesus Christ
+          commanded us to do this in His remembrance in Luke 22vs 19.
         </p>
 
-        <p>
-        Remember this take place every first Sunday of the Month at:
-        </p>
+        <p>Remember this take place every first Sunday of the Month at:</p>
 
-        <p
-        style={sympo}
-        >6pm GMT/BST</p>
-        <p
-        style={sympo}
-        >7pm CET</p>
-        <p
-        style={sympo}
-        >7pm WAT+1</p>
-        <p
-        style={sympo}
-        >9pm MSK</p>
-        <p
-        style={sympo}
-        >1pm EST</p>
-        <p
-        style={sympo}
-        >11am MST</p>
+        <p style={sympo}>6pm GMT/BST</p>
+        <p style={sympo}>7pm CET</p>
+        <p style={sympo}>7pm WAT+1</p>
+        <p style={sympo}>9pm MSK</p>
+        <p style={sympo}>1pm EST</p>
+        <p style={sympo}>11am MST</p>
 
         <p>We would be honoured to have you join us.</p>
 
         <p>Please bring your own bread and Red Wine without alcohol</p>
 
         <p>
-        To  participate in taking the communion please click on zoom link below or enter the Meeting 🆔 and the password 🔑
+          To participate in taking the communion please click on zoom link below
+          or enter the Meeting 🆔 and the password 🔑
         </p>
         <a href="https://us05web.zoom.us/j/89121384589?pwd=wVkJn7myqUvPWuD2YnFtjZbZ8r6t18.1">
           <button
@@ -370,7 +359,7 @@ const EventsApp = () => (
       </div>
     </div>
 
-    <Link to="/events/convention">
+    
       <div
         style={{
           display: "flex",
@@ -383,12 +372,79 @@ const EventsApp = () => (
           </div>
         </div>
 
-        <div>
-          <p>Come join us at our Annual Convention at Hamm, Germany. </p>
-          <p>Dont come alone, Invite someone and be blessed.</p>
+        <div className="conveDiv">
+          <p>Acts 1v 4-8</p>
+
+          <p>
+            But ye shall receive power, after that the Holy Ghost is come upon
+            you: and ye shall be witnesses unto me both in Jerusalem, and in all
+            Judaea, and in Samaria, and unto the uttermost part of the earth.
+          </p>
+          <p>What is the Holy Spirit?</p>
+          <p>
+            Holy Spirit it´s not a what, but He, the 3rd person of the trinity.
+            In Ezekiel chapter 36:25-27 God promise to purify His people, and
+            through a new covenant (Jeremiah 31:31-34); He will give a new
+            spirit and new dispositions to adore Him.
+          </p>
+
+          <p>
+            1° God's promise to pour out His Spirit on all flesh is to eradicate
+            the aridity of His people and bring them to spiritual flourishing.
+            Acts 1 v-8
+          </p>
+
+          <p>
+            2° The indwelling of His Spirit in us, is to enable us to walk in
+            obedience to God and His Word.
+          </p>
+
+          <p>
+            The work of the Holy Spirit is the ability He gives to men to
+            accomplish the tasks for which God has destined them. This consisted
+            of making the believers fit for the service that God has entrusted
+            to them. Actes 1v-8
+          </p>
+
+          <p>
+            3° It is the working power of the Holy Spirit that inspires
+            prophecies. Joel 2 v 28, John 16 v 13.
+          </p>
+
+          <p>Join us at our next Holy Spirit Convention.</p>
+
+          <p>Don’t come alone, Invite someone and be blessed.</p>
+
+          
+          <p
+          style={{
+            textAlign: "center",
+            marginBottom: "1.3rem",
+            fontWeight: "bolder",
+          }}
+        >
+          CONVENTION BEGINS IN{" "}
+        </p>
+
+        <CountdownTimer targetDate={targetDate} />
+
+        <div
+          style={{
+            height: "1.4rem",
+          }}
+        ></div>
+
+        <button style={{ display: "flex", margin: "0 auto" }}>
+          {" "}
+          <a href="https://www.mowdministries.org/events">
+            {" "}
+            CLICK HERE TO PARTICIPATE IN THIS PROGRAM
+          </a>
+        </button>
+
         </div>
       </div>
-    </Link>
+  
 
     <div
       style={{
@@ -453,10 +509,7 @@ const EventsApp = () => (
       </div>
 
       <div>
-        <p
-        style={sympo}>
-          2Timothy 2:1-2, AV)
-          </p>
+        <p style={sympo}>2Timothy 2:1-2, AV)</p>
         <p style={sympo}>
           Thou therefore, my son, be strong in the grace that is in Christ
           Jesus. ‭2 And the things that thou hast heard of me among many
@@ -480,7 +533,7 @@ const EventsApp = () => (
           style={{
             textAlign: "center",
             marginBottom: "1.3rem",
-            fontWeight: 'bolder'
+            fontWeight: "bolder",
           }}
         >
           SYMPOSSIUM BEGINS IN{" "}
@@ -489,19 +542,16 @@ const EventsApp = () => (
         <CountdownTimer targetDate={targetDate} />
 
         <div
-        style={{
-          marginTop: '1.2rem',
-          height: '1.5rem'
-
-        }}
-        >
-
-        </div>
+          style={{
+            marginTop: "1.2rem",
+            height: "1.5rem",
+          }}
+        ></div>
 
         <Link to="/symporeg">
           <button
             style={{
-              marginTop: '1.2rem',
+              marginTop: "1.2rem",
               display: "flex",
               margin: "0 auto",
             }}
@@ -525,37 +575,27 @@ const EventsApp = () => (
       </div>
 
       <div>
-        <p
-        style={sympo}
-        >
+        <p style={sympo}>
           King David sang, praised, and danced before his GOD, and his enemies
           became barren. What your God wants for you is that you come before him
           in his temple with a shear full heart and all you have and just as you
           are with praise, the fruit of your lips. (Psalm 95:2)
         </p>
-        <p
-        style={sympo}
-        >
+        <p style={sympo}>
           The wall of JERICHO crumbles after the children of ISRAEL glorify GOD
           and walk around the wall for 7 days.
         </p>
-        <p
-        style={sympo}
-        >
+        <p style={sympo}>
           The people of GOD were delivered from the hands of their enemies under
           the rule of JOSAPHATTS just by singing and glorifying God. (II
           CHRONICLES 20v21-22; 26-29).
         </p>
-        <p
-        style={sympo}
-        >
+        <p style={sympo}>
           The lame man knew how to keep his miracle by singing, dancing, jumping
           and praising his God in the temple. (Acts 3v8) PAUL and SILAS sang and
           praised God and the prison doors were broken open.
         </p>
-        <p
-        style={sympo}
-        >
+        <p style={sympo}>
           Doors that have been closed for years will open before you when you
           can praise GOD with all your heart, soul and mind.
         </p>
@@ -564,7 +604,7 @@ const EventsApp = () => (
           style={{
             textAlign: "center",
             marginBottom: "1.3rem",
-            fontWeight: 'bolder'
+            fontWeight: "bolder",
           }}
         >
           CONCERT BEGINS IN{" "}
@@ -573,13 +613,10 @@ const EventsApp = () => (
         <CountdownTimer targetDate={targetDate} />
 
         <div
-        style={{
-          height: '1.4rem'
-        }}
-        >
-
-        </div>
-
+          style={{
+            height: "1.4rem",
+          }}
+        ></div>
 
         <button style={{ display: "flex", margin: "0 auto" }}>
           {" "}
