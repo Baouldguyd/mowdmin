ca22a899ef418ca525bd63a937600d32efb526af
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2024-01-10 09:01:58 +0100

    update dot spinner

--- src/Components/Media/Audio/Books.jsx
+++ src/Components/Media/Audio/Books.jsx
@@ -16,6 +16,18 @@ const bookStyle={
 const Books = () => {
   return (
     <div className='aboutpg'>
+
+<div className="dot-spinner">
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+</div>
+
       <style>
         @import url('https://fonts.googleapis.com/css2?family=Mukta&display=swap');
       </style>
