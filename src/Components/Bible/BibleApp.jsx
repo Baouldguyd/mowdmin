e3ee4b401747ade834f9a88511ec39a8e6036c1d
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2024-02-03 13:44:09 +0100

    torah fixed

--- src/Components/Bible/BibleApp.jsx
+++ src/Components/Bible/BibleApp.jsx
@@ -1,70 +1,29 @@
 import React, { useState } from "react";
-// import VerseDisplay from "./VerseDisplay";
 import biblecross from "../../Assets/torah.jpg";
 
 function BibleApp() {
-  // Define the books and chapters for Old and New Testaments
-  const oldTestamentBooks = [ 
+  const oldTestamentBooks = [
     {
       book: "Genesis",
       chapter_length: 50,
     },
-
     {
       book: "Exodus",
-      chapter_length: 40
+      chapter_length: 40,
     },
     {
-      book : "Leviticus",
-      chapter_length: 27
+      book: "Leviticus",
+      chapter_length: 27,
     },
     {
       book: "Numbers",
-      chapter_length: 36
+      chapter_length: 36,
     },
     {
       book: "Deuteronomy",
-      chapter_length: 34
-    }
-
-
-
-    // 'Joshua',
-    // 'Judges',
-    // 'Ruth',
-    // '1 Samuel',
-    // '2 Samuel',
-    // '1 Kings',
-    // '2 Kings',
-    // '1 Chronicles',
-    // '2 Chronicles',
-    // 'Ezra',
-    // 'Nehemiah',
-    // 'Esther',
-    // 'Job',
-    // 'Psalms',
-    // 'Proverbs',
-    // 'Ecclesiastes',
-    // 'Song of Solomon',
-    // 'Isaiah',
-    // 'Jeremiah',
-    // 'Lamentations',
-    // 'Ezekiel',
-    // 'Daniel',
-    // 'Hosea',
-    // 'Joel',
-    // 'Amos',
-    // 'Obadiah',
-    // 'Jonah',
-    // 'Micah',
-    // 'Nahum',
-    // 'Habakkuk',
-    // 'Zephaniah',
-    // 'Haggai',
-    // 'Zechariah',
-    // 'Malachi',
-
-    /* Add more books here */
+      chapter_length: 34,
+    },
+    // Add more books here
   ];
 
   const newTestamentBooks = [
@@ -97,40 +56,19 @@ function BibleApp() {
     "Revelation",
   ];
 
-  // You can access any book in the array by its index, e.g., newTestamentBooks[0] for Matthew.
-
-  const [selectedTestament, setSelectedTestament] = useState("Select A Book");
-  const [selectedBook, setSelectedBook] = useState("Select a Book");
-  const [selectedChapter, setSelectedChapter] = useState("Select a Chapter");
-  // const [selectedVerseNumber, setSelectedVerseNumber] = useState('Select a verse Number');
-  // const [verseText, setVerseText] = useState("");
-  // const [chapterText, setChapterText] = useState("");
-  // const [verseNumber, setVerseNumber] = useState("");
-  // const [bookName, setBookName] = useState("");
+  const [selectedTestament, setSelectedTestament] = useState("nil");
+  const [selectedBook, setSelectedBook] = useState("nil");
+  const [selectedChapter, setSelectedChapter] = useState("nil");
   const [bibleVerses, setBibleVerses] = useState([]);
 
-  // Function to handle verse selection
   const handleVerseSelection = () => {
-    // Construct the verse reference
     const verse = `${selectedBook} ${selectedChapter}`;
     const kjv = "?translation=kjv";
-    // Make an API request to retrieve the Bible verse
 
     fetch(`https://bible-api.com/${verse}${kjv}`)
       .then((response) => response.json())
       .then((data) => {
-        // setVerseText(data.verses[0].text);
-        // setVerseNumber(data.verses[0].verse);
-        // setChapterText(data.verses[0].chapter);
-        // setBookName(data.verses[0].book_name);
         setBibleVerses(data.verses);
-
-        console.log(data);
-        console.log(data.verses);
-        console.log(data.verses[0].book_name);
-        console.log(data.verses[0].chapter);
-        console.log(data.verses[0].verse);
-        console.log(data.verses[0].text);
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -138,12 +76,8 @@ function BibleApp() {
   };
 
   return (
-    <div className="aboutpg bible fade-in appear ">
-      <style>
-        @import
-        url('https://fonts.googleapis.com/css2?family=Mukta&display=swap');
-      </style>
-
+    <div className="aboutpg bible fade-in appear">
+      {/* Add your style import here */}
       <div className="abouthead">
         <div className="aboutheadimg">
           <img src={biblecross} alt="" />
@@ -159,9 +93,9 @@ function BibleApp() {
               value={selectedTestament}
               onChange={(e) => setSelectedTestament(e.target.value)}
             >
-              <option value="nil">Select A Book</option>
-              <option value="Old Testament">Torah</option>
-              {/* <option value="New Testament">New Testament</option> */}
+              <option value="nil">Select A Testament</option>
+              <option value="Old Testament">Old Testament</option>
+              {/* Add New Testament option if needed */}
             </select>
           </div>
           <div>
@@ -170,72 +104,39 @@ function BibleApp() {
               onChange={(e) => setSelectedBook(e.target.value)}
             >
               <option value="nil">Select A Book</option>
-              {selectedTestament === "Old Testament"
-                ? oldTestamentBooks.map((book, index) => (
-                    <option key={index} value={book}>
-                      {book.book}
-                    </option>
-                  ))
-                : newTestamentBooks.map((book) => (
-                    <option key={book} value={book}>
-                      {book}
-                    </option>
-                  ))}
+              {selectedTestament === "Old Testament" &&
+                oldTestamentBooks.map((book, index) => (
+                  <option key={index} value={book.book}>
+                    {book.book}
+                  </option>
+                ))}
             </select>
           </div>
-          
-
-<div>
-  <select
-    value={selectedChapter}
-    onChange={(e) => setSelectedChapter(e.target.value)}
-  >
-    <option value="nil">Select A Chapter</option>
-    {selectedTestament === "Old Testament" &&
-      selectedBook !== "Select a Book" &&
-      oldTestamentBooks
-        .find((book) => book.book === selectedBook).book
-        .chapter_length.map((_, index) => (
-          <option key={index} value={index + 1}>
-            {index + 1}
-          </option>
-        ))}
-  </select>
-</div>
-
-
-
-          {/* <div>
-          <select
-            value={selectedVerseNumber}
-            onChange={(e) => setSelectedVerseNumber(e.target.value)}
-          >
-            <option value="nil">Select A Verse</option>
-            {Array.from({ length: 150 }, (_, i) => (
-              <option key={i + 1} value={i + 1}>
-                {i + 1}
-              </option>
-            ))}
-          </select>
-        </div> */}
+          <div>
+            <select
+              value={selectedChapter}
+              onChange={(e) => setSelectedChapter(e.target.value)}
+            >
+              <option value="nil">Select A Chapter</option>
+              {selectedTestament === "Old Testament" &&
+                selectedBook !== "nil" &&
+                Array.from({ length: oldTestamentBooks.find((b) => b.book === selectedBook).chapter_length }, (_, i) => (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                ))}
+            </select>
+          </div>
           <button onClick={handleVerseSelection}>Open</button>
 
-          {/* <VerseDisplay
-            text={verseText}
-            chapter={chapterText}
-            verseNumber={verseNumber}
-            bookName={bookName}
-          /> */}
-
           {bibleVerses.map((bibleVerse, index) => (
-            <div key={index} value={bibleVerse}>
+            <div key={index}>
               <p> {bibleVerse.book_name}</p>
               <p>
                 {"["} <span>{bibleVerse.chapter}</span> {":"}{" "}
                 <span>{bibleVerse.verse}</span>
                 {" ]"}
               </p>
-
               <p className="verseText">{bibleVerse.text}</p>
             </div>
           ))}
