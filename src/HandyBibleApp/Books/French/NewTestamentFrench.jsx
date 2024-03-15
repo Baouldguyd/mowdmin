7a00ca2b502a6f6c75025e18cfba2d5489458599
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2024-03-15 16:53:21 +0100

    update french

--- src/HandyBibleApp/Books/French/NewTestamentFrench.jsx
+++ src/HandyBibleApp/Books/French/NewTestamentFrench.jsx
@@ -64,7 +64,7 @@ const NewTestamentFrench = () => {
 
         const result = await response.json();
 
-        const booksResponse = await fetch("/get-books/YLT/");
+        const booksResponse = await fetch("https://bolls.life/get-books/YLT/");
         const booksResult = await booksResponse.json();
        
         const selectedBookChapter = bookSelect.find(
